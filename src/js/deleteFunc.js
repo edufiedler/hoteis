0406e5d7b16e90d3b04c9972c75635dafb9a1425
PedroMReno <pedrohmreno@gmail.com>
2022-12-15 22:17:58 -0300

    eh serio

--- src/js/deleteFunc.js
+++ src/js/deleteFunc.js
@@ -1,17 +1,24 @@
-// const btn2 = document.querySelector('#delete')
+function getDadosForm3(){
+    const ifuncionario_cpf = document.querySelector('#funcionario_cpf')
+ 
+    const funcionario = {
+        funcionario_cpf: ifuncionario_cpf.value,
+    }
+    return funcionario
 
-async function enviarDadosParaAPI3(){
-    ifuncionario_cpf = document.querySelector('#funcionario_cpf')
+}
 
-    const resposta = await fetch(`http://localhost:3000/funcionarios/${ifuncionario_cpf.value}`, {
-        method: 'DELETE',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
-    if(resposta.status === 200){
-        window.location.href = 'lista-funcionarios.html'
-    } else console.log('Erro ao apagar funcionário')
-}   
 
+async function enviarDadosParaAPI3(funcionario){
+   const resposta = await fetch(`http://localhost:3000/funcionarios/${funcionario.funcionario_cpf}`, {
+       method: 'DELETE',
+       headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(funcionario)
+   })
+   if(resposta.status === 200){
+       window.location.href = 'lista-funcionarios.html'
+   } else console.log('Erro ao apagar funcionário')
+}  
