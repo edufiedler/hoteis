5acdb8a000a53fb968b2efc78c269366aa388a01
PedroMReno <pedrohmreno@gmail.com>
2022-12-15 21:03:08 -0300

    revert

--- src/js/procurarReq.js
+++ src/js/procurarReq.js
@@ -14,72 +14,59 @@
 function getDadosReq(){
   const irequisicao_id = document.querySelector('#requisicao_id')
   
-  if(irequisicao_id.value.length == 0)
-    return {requisicao_id: ''}
-  else
-    return {requisicao_id: irequisicao_id.value}
+  const requisicao = {
+    requisicao_id: irequisicao_id.value,
+  }
+  
+  return requisicao
 }
 
 async function enviarDadosParaAPI2(requisicao){
-  let resultado;
+  const resp = await fetch(`http://localhost:3000/requisicoes/req/${requisicao.requisicao_id}`, {
+      method: 'GET',
+      headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+      },
+      //body: JSON.stringify(funcionarios)
+  })
 
-  if(requisicao.requisicao_id == '') {
-    const resp = await fetch(`http://localhost:3000/requisicoes`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-    })
-
-    resultado = await resp.json();
-  } else {
-    const resp = await fetch(`http://localhost:3000/requisicoes/req/${requisicao.requisicao_id}`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-    })
-
-    resultado = await resp.json();
-  }
-
-  exibeResul(resultado);
-}   
-
-async function exibeResul(resultado){
+    let resultado = await resp.json();
     document.getElementById("todosOsProdutos").innerHTML = '';
 
 
-    const tabela = `<div class="header-lista">
-    <div class="body-bold">Nome</div>
+      const tabela = `<div class="header-lista">
+      <div class="body-bold">Nome</div>
 
-    <div class="body-bold">CPF do atendente</div>
+      <div class="body-bold">CPF</div>
 
-    <div class="body-bold">Sexo</div>
+      <div class="body-bold">Sexo</div>
 
-    <div class="body-bold">Data de Nascimento</div>
+      <div class="body-bold">Data de Nascimento</div>
 
-    <div class="body-bold">Endereço</div>
+      <div class="body-bold">Endereço</div>
 
-    <div class="body-bold">Salário</div>
+      <div class="body-bold">Salário</div>
 
-    <div class="body-bold">Tipo de Contrato</div>`;
-    document.getElementById("todosOsProdutos").innerHTML=document.getElementById("todosOsProdutos").innerHTML+tabela; 
-    
-    resultado.forEach(funcionario => {
-      var data = funcionario.datanasc;
-      const novoFunc = `
-      <div class="item-lista">
-      <div class="body-lista">${funcionario.nom_pnome} ${funcionario.nom_snome}</div>
-      <div class="body-lista">${funcionario.funcionario_cpf} </div>
-      <div class="body-lista">${funcionario.sexo} </div>
-      <div class="body-lista"> ${data} </div>
-      <div class="body-lista"> ${funcionario.endereco} </div>
-      <div class="body-lista"> ${funcionario.salario} </div>
-      <div class="body-lista"> ${funcionario.tipodecontrato}</div>
-      </div>`
-      document.getElementById("todosOsProdutos").innerHTML=document.getElementById("todosOsProdutos").innerHTML+novoFunc;
-  });
-}
+      <div class="body-bold">Tipo de Contrato</div>`;
+      document.getElementById("todosOsProdutos").innerHTML=document.getElementById("todosOsProdutos").innerHTML+tabela; 
+      
+      resultado.forEach(funcionario => {
+        var data = funcionario.datanasc;
+        const novoFunc = `
+        <div class="item-lista">
+        <div class="body-lista">${funcionario.nom_pnome} ${funcionario.nom_snome}</div>
+        <div class="body-lista">${funcionario.funcionario_cpf} </div>
+        <div class="body-lista">${funcionario.sexo} </div>
+        <div class="body-lista"> ${data} </div>
+        <div class="body-lista"> ${funcionario.endereco} </div>
+        <div class="body-lista"> ${funcionario.salario} </div>
+        <div class="body-lista"> ${funcionario.tipodecontrato}</div>
+        </div>`
+        document.getElementById("todosOsProdutos").innerHTML=document.getElementById("todosOsProdutos").innerHTML+novoFunc;
+    });
+}   
+
+
+
+
