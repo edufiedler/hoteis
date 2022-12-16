2472979a4dc37e288219838ca27bd8bf9aca07dc
PedroMReno <pedrohmreno@gmail.com>
2022-12-15 21:33:26 -0300

    func func

--- src/js/listaFunc.js
+++ src/js/listaFunc.js
@@ -26,7 +26,7 @@ async function consultaTodosOsProdutos(){
         <div class="body-lista"> ${funcionario.salario} </div>
         <div class="body-lista"> ${funcionario.tipodecontrato}</div>
         
-          <a href="editar-funcionario.html">
+          <a href="editar-funcionario.html?funcionario_cpf=${funcionario.funcionario_cpf}">
           <svg
             class="icon"
             width="120"
