40df0f6222b5a108a6a3cc6ea31f14c586ae25b3
PedroMReno <pedrohmreno@gmail.com>
2022-12-15 22:13:50 -0300

    fix delete

--- src/js/deleteFunc.js
+++ src/js/deleteFunc.js
@@ -4,7 +4,12 @@ async function enviarDadosParaAPI3(){
     ifuncionario_cpf = document.querySelector('#funcionario_cpf')
 
     const resposta = await fetch(`http://localhost:3000/funcionarios/${ifuncionario_cpf}`, {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(funcionario)
     })
     if(resposta.status === 200){
         window.location.href = 'lista-funcionarios.html'
