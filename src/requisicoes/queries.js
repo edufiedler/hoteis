ac570a2007b1876ce5549f611a719c92b37461b9
PedroMReno <pedrohmreno@gmail.com>
2022-12-15 22:57:53 -0300

    eh serio

--- src/requisicoes/queries.js
+++ src/requisicoes/queries.js
@@ -4,7 +4,7 @@ const getReqbyFunc = "SELECT * FROM public.requisicao WHERE requisicao_id IN (SE
 const getFuncbyReq = "SELECT * FROM public.funcionario where funcionario_cpf in (SELECT funcionario_cpf FROM public.recebe_requisicao WHERE requisicao_id = $1)";
 const addFuncinReq = "INSERT INTO public.recebe_requisicao(funcionario_cpf, requisicao_id) VALUES ($1, $2)";
 const getReqbyId = "SELECT * FROM public.requisicao WHERE requisicao_id = $1";
-const addReq = "INSER INTO public.requisicao (requisicao_id, tipo, dataabertura, datafechamento, status, textolivre, funcionario_cpf, cliente_id) VALUES ($1, 'F', $2, $3, $4, $5, $6, NULL)";
+const addReq = "INSERT INTO public.requisicao (requisicao_id, tipo, dataabertura, datafechamento, status, textolivre, funcionario_cpf, cliente_id) VALUES ($1, 'F', $2, $3, $4, $5, $6, NULL)";
 const atualizarReq = "UPDATE public.requisicao SET dataabertura = $1, datafechamento = $2, status = $3, textolivre = $4, funcionario_cpf = $5, cliente_id = $6 WHERE requisicao_id = $7";
 const excluirReq = "DELETE FROM public.requisicao WHERE requisicao_id = $1";
 const verificaReq = "SELECT * FROM public.requisicao WHERE requisicao_id = $1";
